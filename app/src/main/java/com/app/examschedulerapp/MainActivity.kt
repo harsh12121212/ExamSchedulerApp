7911f8885afda58c37b590ab9ba19bcdb425052f
vaalewad <vaishnaviaalewad@gmail.com>
2023-04-05 18:08:13 +0530

    navigation and ui setup

--- app/src/main/java/com/app/examschedulerapp/MainActivity.kt
+++ app/src/main/java/com/app/examschedulerapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.app.examschedulerapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+private lateinit var navController: NavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController= navHostFragment.navController
+
     }
 }
