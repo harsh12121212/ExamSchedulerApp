5985255c1e1bb8dec017c27883036719b8b91d6a
hsunilpa <pawarharshada00@gmail.com>
2023-04-06 16:34:07 +0530

    LoginAuth

--- app/src/main/java/com/app/examschedulerapp/LoginFragment.kt
+++ app/src/main/java/com/app/examschedulerapp/LoginFragment.kt
@@ -1,24 +1,47 @@
 package com.app.examschedulerapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.app.examschedulerapp.databinding.FragmentHomeBinding
 import com.app.examschedulerapp.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
 
 
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //for current user direct entry
+    var user = FirebaseAuth.getInstance().currentUser
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnSignin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful)
+
+                        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+                }
+            }
+        }
         return binding.root
     }
 
@@ -26,7 +49,7 @@ class LoginFragment : Fragment() {
 
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvRegister.setOnClickListener{
+        binding.tvRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
 
