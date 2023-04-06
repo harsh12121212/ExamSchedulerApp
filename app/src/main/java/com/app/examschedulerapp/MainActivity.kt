fc31b3392dfe8ee910fcbae3ef2c37755ee75860
hsunilpa <pawarharshada00@gmail.com>
2023-04-06 16:34:07 +0530

    LoginAuth

--- app/src/main/java/com/app/examschedulerapp/MainActivity.kt
+++ app/src/main/java/com/app/examschedulerapp/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.app.examschedulerapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 private lateinit var navController: NavController
+//FirebaseAuth
+private lateinit var firebaseAuth: FirebaseAuth
+
+//for current user direct entry
+var user = FirebaseAuth.getInstance().currentUser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        navController= navHostFragment.navController
 
     }
 }
