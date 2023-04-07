fdd0b87b1dfcb548ebfed1a1154d66072372ca68
vaalewad <vaishnaviaalewad@gmail.com>
2023-04-07 19:20:13 +0530

    Firebase database

--- app/src/main/java/com/app/examschedulerapp/StudentRegisterFragment.kt
+++ app/src/main/java/com/app/examschedulerapp/StudentRegisterFragment.kt
@@ -7,15 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.app.examschedulerapp.databinding.FragmentHomeBinding
+import com.app.examschedulerapp.data.student
 import com.app.examschedulerapp.databinding.FragmentStudentRegisterBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class StudentRegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentStudentRegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var dbRef: DatabaseReference
+    private var name =""
+    private var email=""
+    private var dob=""
+    private var education=""
+    private var technology=""
+    private var exp=""
+    private var techchoice=""
+    private var loc=""
+    private var date=""
+    private var password=""
+    private var type=String()
+    private var uid =""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,10 +47,12 @@ class StudentRegisterFragment : Fragment() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        dbRef= FirebaseDatabase.getInstance().getReference("Student")
+
         binding.btnSignin.setOnClickListener{
             validateData()
+            saveData()
         }
-
     }
 
     private fun validateData() {
@@ -57,6 +74,72 @@ class StudentRegisterFragment : Fragment() {
         }else{
             showSnackBar("Fields cannot be empty")
         }
+    }
+
+    private fun saveData() {
+        name=binding.etStudName.text.toString().trim()
+        email=binding.etStudEmail.text.toString().trim()
+        dob=binding.etStudDob.text.toString().trim()
+        education=binding.etStudEducation.text.toString().trim()
+        technology=binding.etStudTechnology.text.toString().trim()
+        exp=binding.etStudWorkexp.text.toString().trim()
+        techchoice=binding.etStudTechchoice.text.toString().trim()
+        loc=binding.etStudLoc.text.toString().trim()
+        date=binding.etStudExamdate.text.toString().trim()
+        password=binding.etStudPswd.text.toString().trim()
+        type = "STUDENT"
+        uid= FirebaseAuth.getInstance().currentUser?.uid.toString()
+
+
+        if(name.isEmpty()) {
+            binding.etStudName.error = "Please enter name"
+        }
+        if(email.isEmpty()) {
+            binding.etStudEmail.error = "Please enter email"
+        }
+        if (dob.isEmpty()) {
+            binding.etStudDob.error = "Please enter the data"
+        }
+        if (education.isEmpty()) {
+            binding.etStudEducation.error = "Please enter the data"
+        }
+        if (technology.isEmpty()) {
+            binding.etStudTechnology.error = "Please enter the data"
+        }
+        if (exp.isEmpty()) {
+            binding.etStudWorkexp.error = "Please enter the data"
+        }
+        if (techchoice.isEmpty()) {
+            binding.etStudTechchoice.error = "Please enter the data"
+        }
+        if (loc.isEmpty()) {
+            binding.etStudWorkexp.error = "Please enter the data"
+        }
+        if (date.isEmpty()) {
+            binding.etStudExamdate.error = "Please enter the data"
+        }
+        if (password.isEmpty()) {
+            binding.etStudPswd.error = "Please enter the data"
+        }
+        val studentData = student(name,email,dob,education,technology,exp,techchoice,loc,date ,password,type,uid)
+
+        dbRef.child(name).setValue(studentData)
+            .addOnCompleteListener {
+                // To clear the entry
+                binding.etStudName.editableText.clear()
+                binding.etStudEmail.editableText.clear()
+                binding.etStudDob.editableText.clear()
+                binding.etStudEducation.editableText.clear()
+                binding.etStudTechnology.editableText.clear()
+                binding.etStudWorkexp.editableText.clear()
+                binding.etStudTechchoice.editableText.clear()
+                binding.etStudLoc.editableText.clear()
+                binding.etStudExamdate.editableText.clear()
+                binding.etStudPswd.editableText.clear()
+                showSnackBar("Account created successfully")
+            }.addOnFailureListener{ err->
+                showSnackBar("Error${err.message}")
+            }
     }
     private fun showSnackBar(response: String) {
         val snackbar = Snackbar.make(binding.root, response, Snackbar.LENGTH_LONG)
---
+++ app/src/main/java/com/app/examschedulerapp/data/admin.kt
@@ -0,0 +1,11 @@
+package com.app.examschedulerapp.data
+
+data class admin(var name:String?=null,
+                 var email:String?=null,
+                 var city:String?=null,
+                 var centre:String?=null,
+                 var firstslot:String?=null,
+                 var secondslot:String?=null,
+                 var password:String?=null,
+                 var type:String?=null,
+                 var uid :String?=null)
