846db08c3778c3d38e0f44221ca778547897e988
hsunilpa <pawarharshada00@gmail.com>
2023-04-19 18:08:24 +0530

    My Adapter

--- app/src/main/java/com/app/examschedulerapp/AdminFragment.kt
+++ app/src/main/java/com/app/examschedulerapp/AdminFragment.kt
@@ -3,17 +3,25 @@ package com.app.examschedulerapp
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.app.examschedulerapp.data.MyAdapter
 import com.app.examschedulerapp.databinding.FragmentAdminBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AdminFragment : Fragment() {
 
     private lateinit var binding: FragmentAdminBinding
     private lateinit var user: FirebaseAuth
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var Examdata:ArrayList<Examdata>
+    private var db=firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +39,21 @@ class AdminFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_menu, menu)
+
+        recyclerView=findViewById(R.id.recyclerview)
+        recyclerView.layoutManager=LinearLayoutManager(this)
+
+        Userlist= arrayListOf()
+        db=FirebaseFirestore.getInstance()
+        db.collection("centre").get().addOnSuccessListener{
+            if (!it.isEmpty){
+                for (data in it.documents){
+                    val user:User=data.toObject(User:class.java)
+                    uselist.add(User)
+                }
+                recyclerView.adapter=MyAdapter()
+            }
+        }.addOnFailureListener{Toast.makeText(this,it.toString(),Toast.LENGTH_SHORT).show)()}
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
