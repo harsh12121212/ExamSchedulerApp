ed6bbb444ec510bf3267eec23990c0114819c7e4
hsunilpa <pawarharshada00@gmail.com>
2023-06-08 17:13:16 +0530

    new...

--- app/src/main/java/com/app/examschedulerapp/AdminAllRequestsFragment.kt
+++ app/src/main/java/com/app/examschedulerapp/AdminAllRequestsFragment.kt
@@ -5,8 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.app.examschedulerapp.databinding.FragmentAdminAllRequestsBinding
+import com.app.examschedulerapp.databinding.FragmentStudentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class AdminAllRequestsFragment : Fragment() {
+
+    private lateinit var binding: FragmentAdminAllRequestsBinding
+    lateinit var database: DatabaseReference
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/app/examschedulerapp/AdminProfileFragment.kt
+++ app/src/main/java/com/app/examschedulerapp/AdminProfileFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.app.examschedulerapp.data.admin
+import com.app.examschedulerapp.data.student
 import com.app.examschedulerapp.databinding.FragmentAdminProfileBinding
 import com.app.examschedulerapp.databinding.FragmentStudentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
 
 class AdminProfileFragment : Fragment() {
 
@@ -21,6 +23,24 @@ class AdminProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAdminProfileBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
+
+        database = FirebaseDatabase.getInstance().reference
+        fun currentUserReference(): DatabaseReference = database.child("Users").child(auth.currentUser!!.uid)
+        currentUserReference().addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                val admin=dataSnapshot.getValue(admin::class.java)
+
+                binding.tvAdminprofileAdminname.text = "Name : "+ admin!!.name
+                binding.tvAdminprofileAdminemail.text ="Email : "+ admin!!.email
+                binding.tvAdminprofileAdmincity.text ="City of the center: "+ admin.city
+                binding.tvAdminprofileAdmincenter.text ="Centre name : "+ admin!!.centre
+
+            }
+            override fun onCancelled(error: DatabaseError){
+            }
+        })
 
         return binding.root
     }
