b6b36931470b58aeb4c5138180611234d4468f36
vaalewad <vaishnaviaalewad@gmail.com>
2023-06-27 21:32:36 +0530

    Updated MVVM code rebased on the branch

--- app/src/main/java/com/app/examschedulerapp/repository/ExamRepositoryInterface.kt
+++ app/src/main/java/com/app/examschedulerapp/repository/ExamRepositoryInterface.kt
@@ -6,12 +6,9 @@ import com.app.examschedulerapp.data.examdata
 interface ExamRepositoryInterface {
     fun saveExamData(examData: examdata,
                      onSuccess: () -> Unit,
-                     onFailure: (String) -> Unit
-    )
+                     onFailure: (String) -> Unit)
     fun getExamData(studentId: String): LiveData<List<examdata>>
     fun getPendingExamData(callback: (List<examdata>) -> Unit)
     fun updateStatus(item: examdata, status: String, callback: (Boolean) -> Unit)
     fun getExams(callback: (List<examdata>) -> Unit)
 }
-
-
